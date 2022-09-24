aef73035613d7ae1e2ac6c50bc55d82bacaf7b1a
rumahdevid <rumahpayment@gmail.com>
2022-09-24 12:51:29 +0700

    init

---
+++ src/ArtisanJokeServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Rumahdev\Larajoke\Commands;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanJokeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('command.rumahdevid.artisan-joke', function($app) {
+            return $app['Rumahdevid\ArtisanJoke\Commands\JokeCommand'];
+        });
+
+        $this->commands('command.rumahdevid.artisan-joke');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/Commands/JokeCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Rumahdevid\ArtisanJoke\Commands;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+
+class JokeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'joke';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'ini untuk menjalankan joke api.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $client     = new CLient();
+
+        $response   = $client->request('GET','https://icanhazdadjoke.com', [
+            'headers' => [
+                'Accept'    => 'text/plain',
+            ]
+        ]);
+
+        if($response->getStatusCode() != 200) {
+            $this->error('Gagal akses API Joke.');
+            return;
+        }
+            $this->info((string)$response->getBody());
+    }
+}
